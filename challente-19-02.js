fe71e73d8bae01edef30f56bdced28fadd105411
yagel-salazar-g <yagel.salazar@globant.com>
2024-02-22 15:19:51 -0600

    challenge 19/02 completed

---
+++ challente-19-02.js
@@ -0,0 +1,34 @@
+const generateParentheses = (n) => {
+    const currentString = [];
+    const validAnswers = [];
+    
+    const recurse = (forwardParensNeeded, backwardsParensNeeded) => {
+        if (forwardParensNeeded === 0 && backwardsParensNeeded === 0) {
+            validAnswers.push(currentString.join(''));
+        }
+            
+ 
+        if (forwardParensNeeded > 0) {
+            currentString.push('(');
+            recurse(forwardParensNeeded - 1, backwardsParensNeeded);
+ 
+        }
+            
+        if (backwardsParensNeeded > 0 && forwardParensNeeded < backwardsParensNeeded) {
+            currentString.push(')');
+            recurse(forwardParensNeeded, backwardsParensNeeded - 1);
+        }
+ 
+        if (currentString.length > 0) {
+            currentString.pop();
+        }
+    };
+    
+    recurse(n, n);
+    
+    return validAnswers;
+};
+
+console.log('Test 1... ', generateParentheses(3)) // ["((()))","(()())","(())()","()(())","()()()"]
+console.log('Test 2... ', generateParentheses(1)) // ["()"]
+
